182ec33d721d6b1a7ddce14339498b7a84dbb62b
Ekwebelam Williams chuddy <33897714+williamsconcepts@users.noreply.github.com>
2020-05-11 01:58:29 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `CodeBlog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Williams Chuddy`,
+      summary: `who lives and works from Home in Owerri, building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://willscode.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `WilliamsChuddy`,
     },
   },
   plugins: [
